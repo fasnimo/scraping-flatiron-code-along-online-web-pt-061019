34246fc40ac6516da86c5432eb37979a093304cd
Rene Cocom <fasnimo@gmail.com>
2019-08-06 01:11:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,11 +7,17 @@ class Scraper
 
   def get_page
    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-   doc.css(".post")
-   doc.css(".post").first.css("h2").text
-   doc.css(".post").first.css(".date").text
-   doc.css(".post").first.css("p").text
-   binding.pry
+  #  doc.css(".post")
+  #  doc.css(".post").first.css("h2").text
+  #  doc.css(".post").first.css(".date").text
+  #  doc.css(".post").first.css("p").text
+  #  binding.pry
+  doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 
     def print_courses
