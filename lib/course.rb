7c5f52ed0bdb05c4ecd2b9c253d5b2db9fd3cc31
Rene Cocom <fasnimo@gmail.com>
2019-08-06 00:16:19 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,11 +2,11 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize 
-    @@all << self 
+  def initialize
+    @@all << self
   end
 
-  def self.all? 
+  def self.all?
     @@all
   end
 end
